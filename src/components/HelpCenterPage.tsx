addac2d855f13bf3102aea74233b6c75c6e8397a
kareemxamged <kareemxamged@gmail.com>
2025-07-26 07:15:53 +0200

    Deploy 26-07-2025-07-15

--- src/components/HelpCenterPage.tsx
+++ src/components/HelpCenterPage.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { 
   Search,
